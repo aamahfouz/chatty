c3afed2e36522a65d60fa3dff1af8a964f1afdae
AAMAHFOUZ <aamahfouz@gmail.com>
2016-08-17 01:34:57 +0200

    Added Basic navigation bar

---
+++ resources/views/templates/partials/navigation.blade.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="#">Chatty</a>
+		</div>
+		<div class="collapse navbar-collapse">
+			<!-- @if (Auth::check()) -->
+			<ul class="nav navbar-nav">
+				<li><a href="#">Timeline</a></li>
+				<li><a href="#">Friends</a></li>
+			</ul>
+			<form action="#" class="navbar-form navbar-left" role="search">
+				<div class="form-group">
+					<input class="form-control" type="text" name="query" placeholder="Find People"></input>					
+				</div>
+				<button class="btn btn-default" type="submit">Search</button>
+			</form>		
+		<!--@endif -->
+		<ul class="nav navbar-nav navbar-right">
+			<!-- @if (Auth::check()) -->
+			<li><a href="#">AAMAHFOUZ<!-- {{ Auth::user()->getNameOrUsername()}}-->
+			</a></li>
+			<li><a href="#">Update profile</a></li>
+			<li><a href="#">Sign out</a></li>
+			<!-- @else -->
+			<li><a href="#">Sign up</a></li>
+			<li><a href="#">Sign in</a></li>
+			<!-- @endif -->
+		</ul>
+		</div>
+	</div>
+</nav>
